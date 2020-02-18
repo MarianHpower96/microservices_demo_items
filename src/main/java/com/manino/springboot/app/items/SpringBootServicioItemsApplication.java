8f14bd59747d39a13995d97c6656a65ae024c7e7
Marian <mariano.anino96@gmail.com>
2020-02-17 23:14:15 -0300

    ttss

--- src/main/java/com/manino/springboot/app/items/SpringBootServicioItemsApplication.java
+++ src/main/java/com/manino/springboot/app/items/SpringBootServicioItemsApplication.java
@@ -1,6 +1,7 @@
 package com.manino.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +10,4 @@ public class SpringBootServicioItemsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootServicioItemsApplication.class, args);
 	}
-	
 }
